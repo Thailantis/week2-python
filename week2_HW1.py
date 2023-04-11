08ce5737cc34b9d777833d8d21e8e5fd2ed775ed
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-04-10 20:52:43 -0400

    Create week2_HW1.py

---
+++ week2_HW1.py
@@ -0,0 +1,24 @@
+# Cube Number Test... Print out all cubed numbers up to the total value 1000. Meaning that if the cubed number is over 1000 break the loop.
+
+i = 1
+while True:
+    cubed = i ** 3
+    if cubed > 1000:
+        break
+    print(cubed)
+    i += 1
+    
+# Get first prime numbers to 100
+def prime_num(n):
+    for n in range(1, 101, 2):
+        if prime_num(n):
+            print(n)
+          
+ # Take in a users input for their age, if they are younger than 18 print kids, if they're 18 to 65 print adults, else print seniors
+ age = int(input("Enter your age: "))
+    if age < 18:
+        print(kids)
+    elif 18 <= 65:
+        print(adults)
+    else:
+        print(seniors)
